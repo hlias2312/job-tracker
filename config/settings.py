604d6352eff352816edf1ccb9efd95f586d0f6eb
Ilias <esyrmanoglou@gmail.com>
2026-05-26 03:58:19 +0200

    Fix ALLOWED_HOSTS for Railway

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2qfw+sdngge=@o8zr*bg3+qrsx^_a=bavuantj1@^7yy=n@c%8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['job-tracker-production-f6c4.up.railway.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
